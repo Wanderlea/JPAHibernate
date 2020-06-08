8cabf440546af8f87cb29f6a33716dfa9b1e7687
Wanderlea Lodi <wgnoato@gmail.com>
2020-06-08 19:35:34 -0300

    Ultima alteracao

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -1,11 +1,22 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
+@NamedQueries({
+	
+	@NamedQuery(name = "UsuarioPessoa.findAll", query = "select u from UsuarioPessoa u"),
+	@NamedQuery(name = "UsuarioPessoa.findName", query = "select u from UsuarioPessoa u where u.nome = :nome")
+})
 public class UsuarioPessoa {
 	
 	@Id
@@ -18,6 +29,9 @@ public class UsuarioPessoa {
 	private String login;
 	private String senha;
 	private int idade;
+	
+	@OneToMany(mappedBy = "usuarioPessoa", fetch = FetchType.EAGER)
+	private List<TelefoneUser> telefoneUsers;
 	
 	public Long getId() {
 		return id;
@@ -60,6 +74,13 @@ public class UsuarioPessoa {
 	}
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+	
+	public List<TelefoneUser> getTelefoneUsers() {
+		return telefoneUsers;
+	}
+	public void setTelefoneUsers(List<TelefoneUser> telefoneUsers) {
+		this.telefoneUsers = telefoneUsers;
 	}
 	@Override
 	public String toString() {
--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.Test;
 
 import dao.DaoGeneric;
+import model.TelefoneUser;
 import model.UsuarioPessoa;
 
 public class TesteHibernate {
@@ -29,9 +30,9 @@ public class TesteHibernate {
 
 		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 
-		UsuarioPessoa pessoa = daoGeneric.pesquisar(1L, UsuarioPessoa.class);
-		pessoa.setIdade(99);
-		pessoa.setNome("Nome Atualizado");
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(2L, UsuarioPessoa.class);
+		pessoa.setIdade(30);
+		pessoa.setNome("Paulo");
 
 		daoGeneric.updateMerge(pessoa);
 
@@ -119,6 +120,72 @@ public class TesteHibernate {
 		
 		for (UsuarioPessoa usuarioPessoa : list) {
 			System.out.println(usuarioPessoa);
+		}
+		
+	}
+	
+	@Test
+	public void testeQuerySomaIdade(){
+		
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		Long somaIdade = (Long) daoGeneric.getEntityManager().createQuery("select sum(u.idade) from UsuarioPessoa u ").getSingleResult();
+		System.out.println(somaIdade);
+		
+	}
+	
+	@Test
+	public void testeNamedQuery1(){
+		
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		List<UsuarioPessoa> list = daoGeneric.getEntityManager().createNamedQuery("UsuarioPessoa.findAll").getResultList();
+		
+		for (UsuarioPessoa usuarioPessoa : list) {
+			
+			System.out.println(usuarioPessoa);
+		}
+		
+	}
+	
+
+	@Test
+	public void testeNamedQuery2(){
+		
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		List<UsuarioPessoa> list = daoGeneric.getEntityManager().createNamedQuery("UsuarioPessoa.findName")
+				.setParameter("nome", "Paulo")
+				.getResultList();
+		
+		for (UsuarioPessoa usuarioPessoa : list) {
+			
+			System.out.println(usuarioPessoa);
+		}
+		
+	}
+	
+	@Test
+	public void testeGravaTelefone(){
+		
+		DaoGeneric daoGeneric = new DaoGeneric();
+		UsuarioPessoa pessoa = (UsuarioPessoa) daoGeneric.pesquisar(6L, UsuarioPessoa.class);
+		
+		TelefoneUser telefoneUser = new TelefoneUser();
+		telefoneUser.setTipo("Casa");
+		telefoneUser.setNumero("4133257896");
+		telefoneUser.setUsuarioPessoa(pessoa);
+		daoGeneric.salvar(telefoneUser);
+		
+	}
+	
+	@Test
+	public void testeConsultaTelefones(){
+		
+		DaoGeneric daoGeneric = new DaoGeneric();
+		UsuarioPessoa pessoa = (UsuarioPessoa) daoGeneric.pesquisar(6L, UsuarioPessoa.class);
+		
+		for (TelefoneUser fone : pessoa.getTelefoneUsers()) {
+			System.out.println(fone.getNumero());
+			System.out.println(fone.getTipo());
+			System.out.println(fone.getUsuarioPessoa().getNome());
 		}
 		
 	}
